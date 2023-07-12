44ed34d799cf05fdb047a1fee62bd8424abcf0d7
litianlololo <ajxflth@163.com>
2023-07-12 20:06:50 +0800

    完成搭配大部分页面跳转
    新增搭配tag页，用来选择个人搭配、AI、设计师
    实现搭配首页向搭配TAG页跳转
    实现搭配tag页向搭配相册页跳转
    实现搭配相册页向搭配详情页跳转
    以及返回逻辑
    页面很丑，以后再说
    无其他逻辑

--- app/src/main/java/com/example/mojing/Fragments/Fragment_dapei.java
+++ app/src/main/java/com/example/mojing/Fragments/Fragment_dapei.java
@@ -1,13 +1,18 @@
 package com.example.mojing.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
+import com.example.mojing.Dapei_Tag_Activity;
 import com.example.mojing.R;
 
 /**
@@ -19,21 +24,8 @@ public class Fragment_dapei extends Fragment {
     public Fragment_dapei() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment_dapei.
-     */
-    // TODO: Rename and change types and number of parameters
     public static Fragment_dapei newInstance(String param1, String param2) {
         Fragment_dapei fragment = new Fragment_dapei();
-        Bundle args = new Bundle();
-
-        fragment.setArguments(args);
         return fragment;
     }
 
@@ -50,5 +42,23 @@ public class Fragment_dapei extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dapei, container, false);
+    }
+    //在fragment不能直接进行点击事件，需要放到oncreatActivity中,重载该函数即可
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ImageButton imgButton = (ImageButton) getActivity().findViewById(R.id.img_button);
+        EditText score_edit = (EditText) getActivity().findViewById(R.id.score_edit);
+        score_edit.setText("init");
+        imgButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 在这里编写单击事件的逻辑
+                String TAG="Main";
+                score_edit.setText("dianji");
+                Intent intent = new Intent(getActivity(), Dapei_Tag_Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
