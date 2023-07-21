d6029a0c781107da2c4fdc0d6bca669aeda9ed2c
litianlololo <ajxflth@163.com>
2023-07-21 16:57:49 +0800

    实现个人搭配上下装不同尺寸照片裁剪为统一格式

--- app/src/main/java/com/example/mojing/Fragments/Fragment_dapei.java
+++ app/src/main/java/com/example/mojing/Fragments/Fragment_dapei.java
@@ -13,6 +13,7 @@ import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
@@ -31,11 +32,13 @@ import com.example.mojing.Dapei_SetTags_Activity;
 import com.example.mojing.Dapei_Tag_Activity;
 import com.example.mojing.LoginActivity;
 import com.example.mojing.R;
+import com.yalantis.ucrop.UCrop;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -52,7 +55,8 @@ public class Fragment_dapei extends Fragment {
     private Boolean iscombined = false;
     private Button TagBtn;
     private String fileName;
-
+    private Uri croppedImageUri_1; // 保存裁剪后的图片的 URI1
+    private Uri croppedImageUri_2; // 保存裁剪后的图片的 URI2
     public Fragment_dapei() {
         // Required empty public constructor
     }
@@ -146,19 +150,56 @@ public class Fragment_dapei extends Fragment {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == 1 && data != null) {
+//        if (requestCode == 1 && data != null) {
+//            Uri selectedImageUri = data.getData();
+//
+//            try {
+//                Bitmap originalBitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), selectedImageUri);
+//                Bitmap resizedBitmap = Bitmap.createScaledBitmap(originalBitmap, imageButton.getWidth(), imageButton.getHeight(), false);
+//                imageButton.setImageBitmap(resizedBitmap);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        if (requestCode == 1 && resultCode == Activity.RESULT_OK && data != null) {
+//            Uri selectedImageUri = data.getData();
+//
+//            // 使用 UCrop 进行图片裁剪
+//            UCrop.Options options = new UCrop.Options();
+//            options.setToolbarColor(ContextCompat.getColor(getActivity(), R.color.colorPrimary));
+//            options.setStatusBarColor(ContextCompat.getColor(getActivity(), R.color.colorPrimaryDark));
+//            UCrop.of(selectedImageUri, Uri.fromFile(new File(getActivity().getCacheDir(), "cropped_image.jpg")))
+//                    .withAspectRatio(1, 1) // 设置裁剪框的宽高比为1:1
+//                    .withMaxResultSize(200, 200) // 设置裁剪后图片的最大尺寸为200x200像素
+//                    .withOptions(options)
+//                    .start(getActivity(), this); // 传入当前的 Activity 和 Fragment 实例，以便接收裁剪后的结果
+//        } else if (requestCode == UCrop.REQUEST_CROP && resultCode == Activity.RESULT_OK && data != null) {
+//            Uri croppedImageUri = UCrop.getOutput(data);
+//            ImgBtn_1.setImageURI(croppedImageUri);
+//            this.croppedImageUri = croppedImageUri;
+//        }
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK && data != null) {
+            // 从图库选择图片后的裁剪操作结果
             Uri selectedImageUri = data.getData();
-
-            try {
-                Bitmap originalBitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), selectedImageUri);
-                Bitmap resizedBitmap = Bitmap.createScaledBitmap(originalBitmap, imageButton.getWidth(), imageButton.getHeight(), false);
-                imageButton.setImageBitmap(resizedBitmap);
-            } catch (IOException e) {
-                e.printStackTrace();
+            cropImage(selectedImageUri); // 调用裁剪图片的方法
+        } else if (requestCode == UCrop.REQUEST_CROP && resultCode == Activity.RESULT_OK && data != null) {
+            // 从裁剪页面返回的结果
+            Uri croppedImageUri = UCrop.getOutput(data);
+            // 检查裁剪后的图片尺寸是否小于
+            Bitmap croppedBitmap = getBitmapFromUri(croppedImageUri);
+            if (croppedBitmap != null && (croppedBitmap.getWidth() < ImgBtn_1.getWidth() || croppedBitmap.getHeight() < ImgBtn_1.getHeight())) {
+                // 如果尺寸小于200x200像素，则将图片放大到200x200像素
+                Bitmap scaledBitmap = Bitmap.createScaledBitmap(croppedBitmap, ImgBtn_1.getWidth(), ImgBtn_1.getHeight(), true);
+                // 保存裁剪后的图片的 Uri
+                croppedImageUri = saveBitmapToCache(scaledBitmap);
             }
+            imageButton.setImageURI(croppedImageUri); // 设置裁剪后的图片到对应的 ImageButton
+//            if (imageButton == ImgBtn_1) {
+//                this.croppedImageUri_1 = croppedImageUri; // 保存裁剪后的图片的 URI1
+//            } else if (imageButton == ImgBtn_2) {
+//                this.croppedImageUri_2 = croppedImageUri; // 保存裁剪后的图片的 URI2
+//            }
         }
-
-
         downloadBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -238,4 +279,40 @@ public class Fragment_dapei extends Fragment {
             });
         }
     }
+    private void cropImage(Uri selectedImageUri) {
+        // 使用 UCrop 进行图片裁剪
+        UCrop.Options options = new UCrop.Options();
+        options.setToolbarColor(ContextCompat.getColor(getActivity(), R.color.colorPrimary));
+        options.setStatusBarColor(ContextCompat.getColor(getActivity(), R.color.colorPrimaryDark));
+        UCrop.of(selectedImageUri, Uri.fromFile(new File(getActivity().getCacheDir(), "cropped_image.jpg")))
+                .withAspectRatio(1, 1) // 设置裁剪框的宽高比为1:1
+                .withMaxResultSize(ImgBtn_1.getWidth(), ImgBtn_1.getHeight()) // 设置裁剪后图片的最大尺寸为200x200像素
+                .withOptions(options)
+                .start(getActivity(), this); // 传入当前的 Activity 和 Fragment 实例，以便接收裁剪后的结果
+    }
+    // 从 Uri 获取 Bitmap
+    private Bitmap getBitmapFromUri(Uri uri) {
+        try {
+            return MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), uri);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    // 将 Bitmap 保存到缓存并获取其 Uri
+    private Uri saveBitmapToCache(Bitmap bitmap) {
+        File cacheDir = getActivity().getCacheDir();
+        File file = new File(cacheDir, "cropped_image.jpg");
+        try {
+            OutputStream outputStream = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+            outputStream.close();
+            // 获取文件的 Uri
+            return Uri.fromFile(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
