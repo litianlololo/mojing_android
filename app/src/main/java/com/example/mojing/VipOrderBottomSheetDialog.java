8557edb7efec00b57a1818a495b15a3f96132ecd
FallingAKS <2250822598@qq.com>
2023-08-02 21:13:32 +0800

    vip留言完美，下订单界面搭个框架

---
+++ app/src/main/java/com/example/mojing/VipOrderBottomSheetDialog.java
@@ -0,0 +1,25 @@
+package com.example.mojing;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class VipOrderBottomSheetDialog extends BottomSheetDialogFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.vip_order, container, false);
+        // 在这里设置支付页面的布局和逻辑
+        return view;
+    }
+}
